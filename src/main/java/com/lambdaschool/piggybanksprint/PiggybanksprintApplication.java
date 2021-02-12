0ca39c9ef1240f94e74b9fcf5a0e1cfea2a4bf72
JRIGS <raczkowskijonathan@gmail.com>
2021-02-12 10:48:41 -0500

    Inital Commit

---
+++ src/main/java/com/lambdaschool/piggybanksprint/PiggybanksprintApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.piggybanksprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybanksprintApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PiggybanksprintApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/piggybanksprint/PiggybanksprintApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.piggybanksprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggybanksprintApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
